b23872f0a328131fb0bf5735dc045b83283242ae
Andrew <qa@drucode.com>
2013-04-17 18:28:42 +0300

    newcode

--- auto1.py
+++ auto1.py
@@ -13,6 +13,15 @@ username_editname = driver.find_element_by_id("edit-name")
 passw_editpass = driver.find_element_by_id("edit-pass")
 click_login = driver.find_element_by_id("edit-submit")
 
-username_editname.send_keys("ckids_admin")
-passw_editpass.send_keys("ckids_admin")
+username_editname.send_keys("creche_admin")
+passw_editpass.send_keys("creche_admin")
 click_login.click()
+
+# Creating Children
+driver.set_page_load_timeout(30)
+children_link = driver.find_element_by_link_text("Children")
+children_link.click()
+add_new_child_link = driver.find_element_by_class_name("add-content-button")
+add_new_child_link.click()
+
+
