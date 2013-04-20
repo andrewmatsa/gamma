3f3cb74d91f6ed4627bac4192020022f649e156d
Andrew <qa@drucode.com>
2013-04-20 18:00:14 +0300

    gamma

--- randomn_names.py
+++ randomn_names.py
@@ -1,16 +1,4 @@
-import random
-parts = {}
-
-with open('parts.txt', 'r') as f:
-    currentList = []
-    for line in f.readlines():
-        line = line.strip()
-        if line.startswith('[') and line.endswith(']'):
-            currentList = []
-            parts[line[1:-1]] = currentList
-        else:
-            currentList.append(line.strip())
-
-
-for i in xrange(10):
-    print ''.join(random.choice(parts[partName]) for partName in sorted(parts))
+ins = open( "file.txt", "r" )
+array = []
+for line in ins:
+    array.append( line )
