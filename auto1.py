275e9949912df1839dbf553dcc1355d42effbd70
Andrew <qa@drucode.com>
2013-04-22 15:26:12 +0300

    find_by_drop

--- auto1.py
+++ auto1.py
@@ -27,10 +27,17 @@ add_new_child_link.click()
 
 # firstname = driver.find_element_by_class_name("form-text required") #First name
 # lastname = driver.find_element_by_class_name("text-full form-text") # Last name
+driver.set_page_load_timeout(30)
 droproom = driver.find_element_by_class_name("form-select")
+for option in droproom.find_elements_by_tag_name('option'):
+    if option.text == 'Room #3':
+        option.click()
+        print ('You selected:')
+    else:
+        print ('Error: No rooms in this centre!')
 
 
-driver.find_element("14")#room2
+# driver.find_element("14")#room2
 # driver.find_element("15")#room3
 #room
 #sex
