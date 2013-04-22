971c48c6af98c4e4a53cf07279ac06e1a2fb6c45
Andrew <qa@drucode.com>
2013-04-22 23:43:16 +0300

    family

--- auto1.py
+++ auto1.py
@@ -18,68 +18,88 @@ passw_editpass.send_keys("creche_admin")
 click_login.click()
 
 # Creating Children
-driver.set_page_load_timeout(40)
-children_link = driver.find_element_by_link_text("Children")
-children_link.click()
+# driver.set_page_load_timeout(40)
+# children_link = driver.find_element_by_link_text("Children")
+# children_link.click()
+# driver.set_page_load_timeout(30)
+# add_new_child_link = driver.find_element_by_class_name("add-content-button")
+# add_new_child_link.click()
+#
+# driver.set_page_load_timeout(10)
+# firstname = driver.find_element_by_id("edit-title") # First name
+# firstname.send_keys("Children_first1")
+# lastname = driver.find_element_by_id("edit-field-child-second-name-und-0-value") # Last name
+# lastname.send_keys("Children_last1")
+#
+# driver.set_page_load_timeout(40)
+# droproom = driver.find_element_by_id("edit-field-child-room-und")
+# flag = False
+# for option in droproom.find_elements_by_tag_name('option'):
+#     if option.text == 'Room #2': # now system has 3 rooms
+#         option.click()
+#         print ('You selected: ' + option.text)
+#         flag = True
+# if (flag != True):
+#     print ("Error. Can't  find Room")
+#
+# dropsex = driver.find_element_by_id("edit-field-child-sex-und")
+# flag = False
+# for option in dropsex.find_elements_by_tag_name('option'):
+#     if option.text == 'Male': #or Female
+#         option.click()
+#         print ('You selected: ' + option.text)
+#         flag = True
+# if (flag != True):
+#     print ("Error. Can't find option sex")
+#
+# dropphotos = driver.find_element_by_id("edit-field-child-photos-und")
+# flag = False
+# for option in dropphotos.find_elements_by_tag_name('option'):
+#     if option.text == 'Allowed': #or Not Allowed
+#         option.click()
+#         print ('You selected: ' + option.text)
+#         flag = True
+# if (flag != True):
+#     print ("Error. Can't find option photos")
+#
+# autoallergies = driver.find_element_by_id("edit-field-child-allergies-und")
+# autoallergies.send_keys("Plums") #list of all allergies
+#
+# add_new_family = driver.find_element_by_id("edit-field-child-family-und")
+# for option in add_new_family.find_elements_by_tag_name('option'):
+#     if option.text == '== Add New Family for this Child ==':
+#         option.click()
+#         print ('Add family: ' + option.text)
+#
+#
+# # save_changes = driver.find_element_by_id("edit-submit")
+# # save_changes.click()
+#
+# link_cancel = driver.find_element_by_class_name("cancel-button")
+# link_cancel.click()
+
+#---- ADD FAMILY
+driver.get("http://develop.ckids.web.drucode.com/node/add/family")
 driver.set_page_load_timeout(30)
-add_new_child_link = driver.find_element_by_class_name("add-content-button")
-add_new_child_link.click()
-
-driver.set_page_load_timeout(10)
 firstname = driver.find_element_by_id("edit-title") # First name
-firstname.send_keys("Children_first1")
-lastname = driver.find_element_by_id("edit-field-child-second-name-und-0-value") # Last name
-lastname.send_keys("Children_last1")
-
-driver.set_page_load_timeout(40)
-droproom = driver.find_element_by_id("edit-field-child-room-und")
-flag = False
-for option in droproom.find_elements_by_tag_name('option'):
-    if option.text == 'Room #2': # now system has 3 rooms
-        option.click()
-        print ('You selected: ' + option.text)
-        flag = True
-if (flag != True):
-    print ("Error. Can't  find Room")
-
-dropsex = driver.find_element_by_id("edit-field-child-sex-und")
-flag = False
-for option in dropsex.find_elements_by_tag_name('option'):
-    if option.text == 'Male': #or Female
-        option.click()
-        print ('You selected: ' + option.text)
-        flag = True
-if (flag != True):
-    print ("Error. Can't find option sex")
-
-dropphotos = driver.find_element_by_id("edit-field-child-photos-und")
-flag = False
-for option in dropphotos.find_elements_by_tag_name('option'):
-    if option.text == 'Allowed': #or Not Allowed
-        option.click()
-        print ('You selected: ' + option.text)
-        flag = True
-if (flag != True):
-    print ("Error. Can't find option photos")
-
-autoallergies = driver.find_element_by_id("edit-field-child-allergies-und")
-autoallergies.send_keys("Plums") #list of all allergies
-
-add_new_family = driver.find_element_by_id("edit-field-child-family-und")
-for option in add_new_family.find_elements_by_tag_name('option'):
-    if option.text == '== Add New Family for this Child ==':
-        option.click()
-        print ('Add family: ' + option.text)
-
-
-# save_changes = driver.find_element_by_id("edit-submit")
-# save_changes.click()
-
+firstname.send_keys("Parren_first1")
+lastname = driver.find_element_by_id("edit-field-family-second-name-und-0-value") # Last name
+lastname.send_keys("Parrent_last1")
+email_family = driver.find_element_by_id("edit-field-family-email-und-0-email")
+email_family.send_keys("testauto@gmail.com")
+phone = driver.find_element_by_id("edit-field-family-phone-und-0-value")
+phone.send_keys("+38090 9090908 09088080")
+#---add parents / Guardians
+firstname = driver.find_element_by_id("edit-fgm-node-family-form-group-additional-parents-fields-items-0-field-family-addition-first-name-und-value") # First name
+firstname.send_keys("Guardians_first1")
+lastname = driver.find_element_by_id("edit-fgm-node-family-form-group-additional-parents-fields-items-0-field-family-add-second-name-und-value") # Last name
+lastname.send_keys("Guardians_last1")
+phone = driver.find_element_by_id("edit-fgm-node-family-form-group-additional-parents-fields-items-0-field-family-addition-phone-und-value")
+phone.send_keys("+3809809 808909 800 09")
+#doesn't work "add another item button
+# add_another_item = driver.find_elements_by_link_text("Add another item")
+# add_another_item.click()
+# # save_changes = driver.find_element_by_id("edit-submit")
+# # save_changes.click()
 link_cancel = driver.find_element_by_class_name("cancel-button")
 link_cancel.click()
-
-driver.get("http://develop.ckids.web.drucode.com/node/add/family")
-firstname = driver.find_element_by_id("edit-title") # First name
-firstname.send_keys("Children_first1")
-lastname = driver.find_element_by_id("edit-field-child-second-name-und-0-value") # Last name
-lastname.send_keys("Children_last1")
