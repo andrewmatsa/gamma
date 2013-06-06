1d06ca23a5eff5f8777175ca400ae5f13f43752c
Andrew <qa@drucode.com>
2013-06-06 17:58:39 +0300

    doesn't work with title first name deleted loadpage added wait=webdriverwait(10

--- auto1.py
+++ auto1.py
@@ -48,6 +48,7 @@ print green ('Login as: "creche_admin / creche_admin"')
 click_login.click()
 
 #------ADD Children
+print blue("Click link 'Children'")
 driver.set_page_load_timeout(300)
 children_link = driver.find_element_by_link_text("Children")
 children_link.click()
@@ -55,203 +56,216 @@ driver.set_page_load_timeout(300)
 add_new_child_link = driver.find_element_by_class_name("add-content-button")
 add_new_child_link.click()
 
-driver.set_page_load_timeout(300)
+print blue("Creating child...")
+
 firstname = driver.find_element_by_id("edit-title")
+wait = WebDriverWait(driver, 10)
 firstname.send_keys("Children_first2")
 lastname = driver.find_element_by_id("edit-field-child-second-name-und-0-value")
 lastname.send_keys("Children_last2")
-print green ('Create child: "Children_first2 / Children_last2"')
+print green('Created child name: "Children_first2 / Children_last2"')
 
 
 # region Some commented a code
 
-#
-# driver.set_page_load_timeout(40)
-# droproom = driver.find_element_by_id("edit-field-child-room-und")
-# flag = False
-# for option in droproom.find_elements_by_tag_name('option'):
-#     if option.text == 'Room #2': # now system has 3 rooms
-#         option.click()
-#         print green('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print red("Error. Can't  find Room")
-#
-# dropsex = driver.find_element_by_id("edit-field-child-sex-und")
-# flag = False
-# for option in dropsex.find_elements_by_tag_name('option'):
-#     if option.text == 'Male': #or Female
-#         option.click()
-#         print green('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print red("Error. Can't find option sex")
-#
-# dropphotos = driver.find_element_by_id("edit-field-child-photos-und")
-# flag = False
-# for option in dropphotos.find_elements_by_tag_name('option'):
-#     if option.text == 'Allowed': #or Not Allowed
-#         option.click()
-#         print green('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print red("Error. Can't find option photos")
-#
-# autoallergies = driver.find_element_by_id("edit-field-child-allergies-und")
-# autoallergies.send_keys("Plums") #list of all allergies
-#
-# add_new_family = driver.find_element_by_id("edit-field-child-family-und")
-# for option in add_new_family.find_elements_by_tag_name('option'):
-#   if option.text == '== Add New Family for this Child ==':
-#         option.click()
-#         print green('Add family: ' + option.text)
-#
-#
+
+driver.set_page_load_timeout(40)
+droproom = driver.find_element_by_id("edit-field-child-room-und")
+flag = False
+for option in droproom.find_elements_by_tag_name('option'):
+    if option.text == 'Room #2': # now system has 3 rooms
+        option.click()
+        print green('You selected: ' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't  find Room")
+
+dropsex = driver.find_element_by_id("edit-field-child-sex-und")
+flag = False
+for option in dropsex.find_elements_by_tag_name('option'):
+    if option.text == 'Male': #or Female
+        option.click()
+        print green('You selected: ' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't find option sex")
+
+dropphotos = driver.find_element_by_id("edit-field-child-photos-und")
+flag = False
+for option in dropphotos.find_elements_by_tag_name('option'):
+    if option.text == 'Allowed': #or Not Allowed
+        option.click()
+        print green('You selected: ' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't find option photos")
+
+autoallergies = driver.find_element_by_id("edit-field-child-allergies-und")
+autoallergies.send_keys("Plums") #list of all allergies
+
+add_new_family = driver.find_element_by_id("edit-field-child-family-und")
+for option in add_new_family.find_elements_by_tag_name('option'):
+  if option.text == '== Add New Family for this Child ==':
+        option.click()
+        print green('Add family: ' + option.text)
+
+
 # save_changes = driver.find_element_by_id("edit-submit")
 # save_changes.click()
-#
-# link_cancel = driver.find_element_by_class_name("cancel-button")
-# link_cancel.click()
-#
-# #---- ADD FAMILY
-# driver.get("http://develop.ckids.web.drucode.com/node/add/family")
-#
-# if driver.get == False:
-#     print ('Cant open this page' + driver.get)
-# driver.set_page_load_timeout(40)
-# firstname = driver.find_element_by_id("edit-title") # First name
-# firstname.send_keys("Parren_first1")
-# lastname = driver.find_element_by_id("edit-field-family-second-name-und-0-value") # Last name
-# lastname.send_keys("Parrent_last1")
-# email_family = driver.find_element_by_id("edit-field-family-email-und-0-email")
-# email_family.send_keys("testauto@gmail.com")
-# phone = driver.find_element_by_id("edit-field-family-phone-und-0-value")
-# phone.send_keys("+38090 9090908 09088080")
-# #---add parents / Guardians
-# firstname = driver.find_element_by_id("edit-fgm-node-family-form-group-additional-parents-fields-items-0-field-family-addition-first-name-und-value") # First name
-# firstname.send_keys("Guardians_first1")
-# lastname = driver.find_element_by_id("edit-fgm-node-family-form-group-additional-parents-fields-items-0-field-family-add-second-name-und-value") # Last name
-# lastname.send_keys("Guardians_last1")
-# phone = driver.find_element_by_id("edit-fgm-node-family-form-group-additional-parents-fields-items-0-field-family-addition-phone-und-value")
-# phone.send_keys("+3809809 808909 800 09")
-# #doesn't work "add another item button
-# # add_another_item = driver.find_elements_by_link_text("Add another item")
-# # add_another_item.click()
-#
-# # # save_changes = driver.find_element_by_id("edit-submit")
-# # # save_changes.click()
-# link_cancel = driver.find_element_by_class_name("cancel-button")
-# link_cancel.click()
-#
-#
-# #---- ADD Carer
-# driver.get("http://develop.ckids.web.drucode.com/node/add/carer")
-# driver.set_page_load_timeout(40)
-# firstname = driver.find_element_by_id("edit-title") # First name
-# firstname.send_keys("Carer_first1")
-# lastname = driver.find_element_by_id("edit-field-carer-second-name-und-0-value") # Last name
-# lastname.send_keys("Carer_last1")
-# email_family = driver.find_element_by_id("edit-field-carer-email-und-0-email")
-# email_family.send_keys("testauto@gmail.com")
-#
-# dropsexcarer = driver.find_element_by_id("edit-field-carer-sex-und")
-# flag = False
-# for option in dropsexcarer.find_elements_by_tag_name('option'):
-#     if option.text == 'Male': #or Female
-#         option.click()
-#         print ('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print ("Error. Can't find option sex")
-#
-# # photocarer = driver.find_element_by_id("edit-field-carer-photo-und-0-upload")
-# # photocarer.click()
-# # driver.findElement(By.xpath("//div[text()='Upload Photos/Video']following-sibling::div/input")).sendKeys("C:\\MyPhoto.jpg");
-# # driver.find_element_by_id("edit-field-carer-photo-und-0-upload").sendKeys("D:/stock-photo-boys-and-girls-running-towards-ball-59018779.jpg")
-#
-# droproom = driver.find_element_by_id("edit-field-carer-room-mon-und")
-# flag = False
-# for option in droproom.find_elements_by_tag_name('option'):
-#     if option.text == 'Not Working':
-#         option.click()
-#         print ('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print ("Error. Can't find room: " + option.text)
-#
-# droproom = driver.find_element_by_id("edit-field-carer-room-tue-und")
-# flag = False
-# for option in droproom.find_elements_by_tag_name('option'):
-#     if option.text == 'Room #1':
-#         option.click()
-#         print ('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print ("Error. Can't find room: " + option.text)
-#
-# droproom = driver.find_element_by_id("edit-field-carer-room-wed-und")
-# flag = False
-# for option in droproom.find_elements_by_tag_name('option'):
-#     if option.text == 'Room #2':
-#         option.click()
-#         print ('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print ("Error. Can't find room: " + option.text)
-#
-# droproom = driver.find_element_by_id("edit-field-carer-room-thu-und")
-# flag = False
-# for option in droproom.find_elements_by_tag_name('option'):
-#     if option.text == 'Room #3':
-#         option.click()
-#         print ('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print ("Error. Can't find room: " + option.text)
-#
-# droproom = driver.find_element_by_id("edit-field-carer-room-fri-und")
-# flag = False
-# for option in droproom.find_elements_by_tag_name('option'):
-#     if option.text == 'Room #4':
-#         option.click()
-#         print ('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print ("Error. Can't find room: " + option.text)
-#
-# droproom = driver.find_element_by_id("edit-field-carer-room-sat-und")
-# flag = False
-# for option in droproom.find_elements_by_tag_name('option'):
-#     if option.text == 'Room #5':
-#         option.click()
-#         print ('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#    print ("Error. Can't find room: " + option.text)
-#
-# droproom = driver.find_element_by_id("edit-field-carer-room-sun-und")
-# flag = False
-# for option in droproom.find_elements_by_tag_name('option'):
-#     if option.text == 'Room #6':
-#         option.click()
-#         print ('You selected: ' + option.text)
-#         flag = True
-# if (flag != True):
-#     print ("Error. Can't find room: " + option.text)
-#
-# # # # save_changes = driver.find_element_by_id("edit-submit")
-# # # # save_changes.click()
-# driver.find_element_by_class_name("cancel-button").click()
+
+driver.find_element_by_class_name("cancel-button").click()
+
+
+#---- ADD FAMILY
+print blue("Click 'Family'")
+driver.get("http://develop.ckids.web.drucode.com/node/add/family")
+
+if driver.get == False:
+    print ('Cant open this page' + driver.get)
+driver.set_page_load_timeout(40)
+firstname = driver.find_element_by_id("edit-title") # First name
+firstname.send_keys("Parren_first1")
+lastname = driver.find_element_by_id("edit-field-family-second-name-und-0-value") # Last name
+lastname.send_keys("Parrent_last1")
+email_family = driver.find_element_by_id("edit-field-family-email-und-0-email")
+email_family.send_keys("testauto@gmail.com")
+phone = driver.find_element_by_id("edit-field-family-phone-und-0-value")
+phone.send_keys("+38090 9090908 09088080")
+#---add parents / Guardians
+firstname = driver.find_element_by_id("edit-fgm-node-family-form-group-additional-parents-fields-items-0-field-family-addition-first-name-und-value") # First name
+firstname.send_keys("Guardians_first1")
+lastname = driver.find_element_by_id("edit-fgm-node-family-form-group-additional-parents-fields-items-0-field-family-add-second-name-und-value") # Last name
+lastname.send_keys("Guardians_last1")
+phone = driver.find_element_by_id("edit-fgm-node-family-form-group-additional-parents-fields-items-0-field-family-addition-phone-und-value")
+phone.send_keys("+3809809 808909 800 09")
+#doesn't work "add another item button
+# add_another_item = driver.find_elements_by_link_text("Add another item")
+# add_another_item.click()
+
+# # save_changes = driver.find_element_by_id("edit-submit")
+# # save_changes.click()
+link_cancel = driver.find_element_by_class_name("cancel-button")
+link_cancel.click()
+
+
+#---- ADD Carer
+print blue("Click 'Carer'")
+driver.get("http://develop.ckids.web.drucode.com/node/add/carer")
+driver.set_page_load_timeout(40)
+print blue("Creating 'Carer'")
+firstname = driver.find_element_by_id("edit-title") # First name
+firstname.send_keys("Carer_first1")
+lastname = driver.find_element_by_id("edit-field-carer-second-name-und-0-value") # Last name
+lastname.send_keys("Carer_last1")
+email_family = driver.find_element_by_id("edit-field-carer-email-und-0-email")
+email_family.send_keys("testauto@gmail.com")
+
+dropsexcarer = driver.find_element_by_id("edit-field-carer-sex-und")
+flag = False
+for option in dropsexcarer.find_elements_by_tag_name('option'):
+    if option.text == 'Male': #or Female
+        option.click()
+        print green('You selected: ' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't find option sex")
+
+print blue("Selecting photo...")
+click_browse_carer = driver.find_element_by_id("edit-field-carer-photo-und-0-upload")
+click_browse_carer.send_keys("D:\\stock-photo-woman-smiling-and-standing-in-the-front-of-the-computer-class-114470674.jpg")
+driver.find_element_by_id("edit-field-carer-photo-und-0-upload-button").click()
+
+print green("Added photo 'Carer'")
+wait = WebDriverWait(driver, 10)
+wait.until(lambda driver: driver.find_element_by_xpath("//*[@id='edit-field-carer-photo-und-0-remove-button']"))
+
+
+
+
+
+
+
+
+droproom1 = driver.find_element_by_id("edit-field-carer-room-mon-und")
+flag = False
+for option in droproom1.find_elements_by_tag_name('option'):
+    if option.text == 'Not Working':
+        option.click()
+        print green('You selected: "Mon"' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't find room: " + option.text)
+
+droproom2 = driver.find_element_by_id("edit-field-carer-room-tue-und")
+flag = False
+for option in droproom2.find_elements_by_tag_name('option'):
+    if option.text == 'Room #1':
+        option.click()
+        print green('You selected: "Tue"' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't find room: " + option.text)
+
+droproom3 = driver.find_element_by_id("edit-field-carer-room-wed-und")
+flag = False
+for option in droproom3.find_elements_by_tag_name('option'):
+    if option.text == 'Room #2':
+        option.click()
+        print green('You selected: "Wed"' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't find room: " + option.text)
+
+droproom4 = driver.find_element_by_id("edit-field-carer-room-thu-und")
+flag = False
+for option in droproom4.find_elements_by_tag_name('option'):
+    if option.text == 'Room #3':
+        option.click()
+        print green('You selected: "Thu"' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't find room: " + option.text)
+
+droproom5 = driver.find_element_by_id("edit-field-carer-room-fri-und")
+flag = False
+for option in droproom5.find_elements_by_tag_name('option'):
+    if option.text == 'Room #4':
+        option.click()
+        print green('You selected: "Fri"' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't find room: " + option.text)
+
+droproom6 = driver.find_element_by_id("edit-field-carer-room-sat-und")
+flag = False
+for option in droproom6.find_elements_by_tag_name('option'):
+    if option.text == 'Room #5':
+        option.click()
+        print green('You selected: "Sat"' + option.text)
+        flag = True
+if (flag != True):
+   print red("Error. Can't find room: " + option.text)
+
+droproom7 = driver.find_element_by_id("edit-field-carer-room-sun-und")
+flag = False
+for option in droproom7.find_elements_by_tag_name('option'):
+    if option.text == 'Room #6':
+        option.click()
+        print green('You selected: "Sun"' + option.text)
+        flag = True
+if (flag != True):
+    print red("Error. Can't find room: " + option.text)
+
+# # # driver.find_element_by_id("edit-submit").click()
+
+driver.find_element_by_class_name("cancel-button").click()
 
 # endregion
 
 #-------ADD PHOTOS
-print blue("Adding photo...")
+print blue("Adding photo... Click 'Photos'")
 driver.get("http://develop.ckids.web.drucode.com/centre/xyz-childrens-creche/photos")
-driver.set_page_load_timeout(300)
-addphoto = driver.find_element_by_class_name("add-content-button")
-addphoto.click()
-
+driver.find_element_by_class_name("add-content-button").click()
 WebDriverWait(driver, 10)
 title = driver.find_element_by_id("edit-title")
 title.send_keys("test photo1")
