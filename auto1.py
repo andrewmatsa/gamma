2c0978843c85c4ac3a86bfb7eb42211fbff3749a
Andrew <qa@drucode.com>
2013-06-11 18:32:39 +0300

    finished event

--- auto1.py
+++ auto1.py
@@ -335,3 +335,24 @@ driver.find_element_by_class_name("cancel-button").click()
 
 
 # endregion
+
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("selenium")
+        elem.send_keys(Keys.RETURN)
+        self.assertIn("Google", driver.title)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
